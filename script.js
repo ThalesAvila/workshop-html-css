fc0d571fec58137cd3b9b184fbacd3e5019e8dd2
ThalesAvila <dev.thales.avila@gmail.com>
2023-10-23 23:28:52 +0100

    Implementando o tours component

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const createAlbumComponent = (album) => {
-  console.log(album.stars)
+  console.log(album.stars);
   // div.imggroup
   const albumComponent = document.createElement("div");
   albumComponent.classList.add("imggroup");
@@ -24,11 +24,11 @@ const createAlbumComponent = (album) => {
   blockquote.appendChild(br);
 
   // div.imggroup > figure > blockquote > img
-  for(i = 0; i < album.stars; i++) {
-      const blockquoteImg = document.createElement("img");
-      blockquoteImg.src = "https://assets.codepen.io/296057/fem-star.svg";
-      blockquoteImg.classList.add("star");
-      blockquote.appendChild(blockquoteImg);
+  for (i = 0; i < album.stars; i++) {
+    const blockquoteImg = document.createElement("img");
+    blockquoteImg.src = "https://assets.codepen.io/296057/fem-star.svg";
+    blockquoteImg.classList.add("star");
+    blockquote.appendChild(blockquoteImg);
   }
 
   // div.imggroup > figure > figcaption
@@ -55,3 +55,52 @@ fetch("https://api.deezer.com/artist/134790/albums")
       albumsContainer.appendChild(createAlbumComponent(album));
     });
   });
+
+const createTourComponent = (tour) => {
+  // p
+  const p = document.createElement("p");
+  // p > span.date
+  const span = document.createElement("span");
+  span.classList.add("date");
+  span.innerText = tour.date;
+  p.append(span);
+  // p > br
+  const br = document.createElement("br");
+  p.append(br);
+  // p > span.club
+  const span2 = document.createElement("span");
+  span2.classList.add("club");
+  span2.innerText = tour.club;
+  p.append(span2);
+  // p > br
+  const br2 = document.createElement("br");
+  p.append(br2);
+  p.append(tour.city);
+
+  return p;
+};
+
+fetch("https://api.deezer.com/artist/134790/tours")
+  .then((response) => response.json())
+  .then((response) => {
+    const tours = response;
+
+    tours.forEach((tour) => {
+      const main = document.getElementsByTagName("main")[0];
+      main.append(createTourComponent(tour));
+    });
+  });
+
+// const main = document.getElementsByTagName("main")[0];
+// console.log(main);
+// const tour1 = {
+//   date: "31 July 2023",
+//   club: "The IVY",
+//   city: "Oakheart, CA",
+// };
+// main.append(createTourComponent(tour1));
+// main.append(createTourComponent(tour1));
+// main.append(createTourComponent(tour1));
+// main.append(createTourComponent(tour1));
+// main.append(createTourComponent(tour1));
+// main.append(createTourComponent(tour1));
